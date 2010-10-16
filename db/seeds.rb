f1c58f12f2e3cdd518182315e15d2b3ae8d22862
Ilya Zayats <somebody32@gmail.com>
2010-10-16 10:47:33 +0400

    added seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+#
+#
+u   = User.create(:email => "test@logbook.me", :password => "123456", :password_confirmation => "123456")
+app = u.applications.create(:name => "Test App")
+FASILITIES = %w(important geocode kitchen server garage web)
+
+
+100.times do
+  payload = Hash[*Faker::Lorem.words(6).collect { |v| [v, v*2] }.flatten]
+  app.entries.create(:severity => Entry::SEVERITY_TYPES.rand(), :facility => FASILITIES.rand(), :payload => payload)
+end
+
+
