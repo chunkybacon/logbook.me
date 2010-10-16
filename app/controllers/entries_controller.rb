63fa0773a8a20d538ef31c3db191e62ac6c0c8ad
Ilya Zayats <somebody32@gmail.com>
2010-10-16 22:57:21 +0400

    updated entry controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -6,7 +6,7 @@ class EntriesController < ApplicationController
 
   def index
     application = current_user.applications.find(params[:application_id])
-    @entries = application.entries.order(:created_at).all
+    @entries = Entry.where(:application_id => application.id).order(:created_at).all
     @facilities = @entries.collect(&:facility).uniq
   end
 
@@ -14,7 +14,8 @@ class EntriesController < ApplicationController
     @application = Application.find_by_api_key(params[:api_key])
     if @application
       params[:entries].values.each do |entry|
-        Entry.create!(entry, :application_id => @application.id)
+        entry.merge!(:application_id => @application.id)
+        Entry.create!(entry)
       end
       head :ok
     else
