cb9eadda00ff2054ea5ebaa5f7e3fb079f293d35
eugenebolshakov <eugene.bolshakov@gmail.com>
2010-10-16 13:49:10 +0400

    Added entries#create

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,4 +4,16 @@ class EntriesController < ApplicationController
     @entries = Entry.all
     @facilities = @entries.collect(&:facility).uniq
   end
+
+  def create
+    @application = Application.find_by_api_key(params[:api_key])
+    if @application
+      params[:entries].each do |entry|
+        @application.entries.create!(entry)
+      end
+      head :ok
+    else
+      render :nothing => true, :status => 401
+    end
+  end
 end
