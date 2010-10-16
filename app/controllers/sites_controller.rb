3c3d3b41605816ea6a104a0f00d7f21b14e8a855
Maxim Filatov <pipopolam@gmail.com>
2010-10-16 19:33:20 +0400

    Redirect to dashboard after login

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,5 +1,13 @@
 class SitesController < ApplicationController
+  before_filter :authenticate_user!, :only => "dashboard"
+  
   def index
+    if current_user
+      render :dashboard
+    else
+      render :index
+    end
+    return
   end
 
   def dashboard
