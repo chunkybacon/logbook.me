11b42985b00c147d542c15854bc9c8f56558087a
Ilya Zayats <somebody32@gmail.com>
2010-10-17 13:09:45 +0400

    cleaned up applications forms

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationsController < InheritedResources::Base
   before_filter :authenticate_user!
+
+  def create
+    create! do |format|
+      format.html { redirect_to applications_path }
+    end    
+  end
+
+  def update
+    update! do |format|
+      format.html { redirect_to applications_path }
+    end
+  end  
   
   protected
   def begin_of_association_chain
