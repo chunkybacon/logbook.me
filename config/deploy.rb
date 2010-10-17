e7048bf15d14e14d9d747026a6e110e683d14a9f
eugenebolshakov <eugene.bolshakov@gmail.com>
2010-10-17 09:54:54 +0400

    Fixed deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :scm, :git
 set :deploy_via, :remote_cache
 set :git_enable_submodules, false
 set :use_sudo, false
-set :branch, :mongo
+set :branch, :master
 
 role :web, "logbook.me"                          # Your HTTP server, Apache/etc
 role :app, "logbook.me"                          # This may be the same as your `Web` server
