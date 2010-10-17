b321450afba0a8caa0830a568bf2c13c1bcffeba
eugenebolshakov <eugene.bolshakov@gmail.com>
2010-10-17 22:13:46 +0400

    More tweaks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def simple_human_friendly_payload(hash)
+    hash.map { |el| "#{el.first}: #{el.last}" }.join(', ')
+  end
+
+  def human_friendly_payload(hash)
+    hash.map { |el| "<span class='key'>#{el.first}</span>: <span class='value'>#{el.last}</span>" }.
+      join(',<br />')
+  end
 end
